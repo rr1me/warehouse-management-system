c25dd84c6e8497c527fbc031f8908033bfe616d2
rime <s1337228@yandex.ru>
2022-10-25 20:19:46 +0300

    modal wrapper

--- frontend/src/Components/RelativeModal/RelativeModal.js
+++ frontend/src/Components/RelativeModal/RelativeModal.js
@@ -1,5 +1,5 @@
 ﻿import './RelativeModal.css'
-import {memo, useRef} from "react";
+import {memo, useRef, useState} from "react";
 import {useEffect} from "react";
 import {makeCloseEvent} from "../Properties/makeCloseEvent";
 
@@ -7,49 +7,49 @@ const RelativeModal = memo(({state, doubleWrap, children, id, modalStyle, itemCl
 
     const modalRef = useRef();
     
+    const [height, setHeight] = useState();
+    const [left, setLeft] = useState((newLogicRef && newLogicRef.current) ? newLogicRef.current.offsetLeft : 0);
     useEffect(() => {
         if (setOpen) {
             makeCloseEvent(id, setOpen);
         }
-
+        if (modalRef.current) setHeight(modalRef.current.getBoundingClientRect().height);
+        
+        
     });
-    // let crds;
-    // if (newLogicRef){
-    //     const current = newLogicRef.current;
-    //     if (!current) return;
-    //     crds = current.getBoundingClientRect();
-    //     console.log(modalRef.current.getBoundingClientRect());
-    //d
-    // }
-    console.log(modalRef.current);
+    
+    const getHeight = () => {
+        console.log(modalRef);
+        return -height;
+    }
+    
     if (state){
         return (
-            <div className={'trying'} ref={modalRef} style={{top: '-' + (modalRef.current ? modalRef.current.getBoundingClientRect().height : '0') + 'px', left: (newLogicRef.current ? newLogicRef.current.getBoundingClientRect().x : '0') + 'px'}}>
-                {/*{newLogic */}
-                {/*    */}
-                {/*    ? doubleWrap ? (children.length > 1 ? children.map((value, index) => {*/}
-                {/*            return (*/}
-                {/*                <div key={index} className={itemClassname}>*/}
-                {/*                    {value}*/}
-                {/*                </div>*/}
-                {/*            )*/}
-                {/*        }) : <div className={itemClassname}>{children}</div>) : children */}
-                {/*    */}
-                {/*:*/}
-                {/*    */}
-                {/*    <div id={id} className='relativeModal' style={modalStyle} onClick={onClick}>*/}
-                {/*        {doubleWrap ? (children.length > 1 ? children.map((value, index) => {*/}
-                {/*            return (*/}
-                {/*                <div key={index} className={itemClassname}>*/}
-                {/*                    {value}*/}
-                {/*                </div>*/}
-                {/*            )*/}
-                {/*        }) : <div className={itemClassname}>{children}</div>) : children}*/}
-                {/*    </div>*/}
-                {/*}*/}
-                <div>d</div>
-                <div>d</div>
-                <div>d</div>
+            <div style={{position: 'relative'}} ref={modalRef}>
+                <div className={'trying'} style={{top: getHeight() + 'px', left: left + 'px'}}>
+                    {newLogic
+
+                        ? doubleWrap ? (children.length > 1 ? children.map((value, index) => {
+                            return (
+                                <div key={index} className={itemClassname}>
+                                    {value}
+                                </div>
+                            )
+                        }) : <div className={itemClassname}>{children}</div>) : children
+
+                        :
+
+                        <div id={id} className='relativeModal' style={modalStyle} onClick={onClick}>
+                            {doubleWrap ? (children.length > 1 ? children.map((value, index) => {
+                                return (
+                                    <div key={index} className={itemClassname}>
+                                        {value}
+                                    </div>
+                                )
+                            }) : <div className={itemClassname}>{children}</div>) : children}
+                        </div>
+                    }
+                </div>
             </div>
         )
     }
