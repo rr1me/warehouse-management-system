f82f0bd79c8d1ecc749204552d89e38a61e12cbc
rime <s1337228@yandex.ru>
2022-11-11 18:36:20 +0300

    add simplebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,35 +9,31 @@ import DriversGrid from "./Components/Drivers/DriversGrid";
 import Transits from "./Components/Transits/Transits";
 import TransitPage from "./Components/Transits/TransitPage/TransitPage";
 
-// import 'overlayscrollbars/overlayscrollbars.css';
-import {OverlayScrollbarsComponent} from "overlayscrollbars-react";
-// import { OverlayScrollbars } from 'overlayscrollbars';
+import SimpleBar from "simplebar-react";
+import 'simplebar-react/dist/simplebar.min.css';
 
 export default function App() {
   return (
-          // <CompactRouter/>
-    <CompactRouter/>
+      <SimpleBar style={{height: '100%'}}>
+          <Router>
+              <Routes>
+                  <Route path="/login" element={<Login/>}/>
+
+                  <Route path="/" element={<Private Component={<h1>home</h1>}/> }/>
+                  <Route path="/cargo" element={<Private Component={<Cargo/>}/> }/>
+                  <Route path="/drivers" element={<Private Component={<DriversGrid/>}/> }/>
+
+                  <Route path="/transits" element={<Private Component={<Transits/>}/> }/>
+                  {["/transits/add", "/transits/:id"].map(path => (
+                      <Route key='transit' path={path} element={<Private Component={<TransitPage/>}/> }/>
+                  ))}
+
+                  <Route path="*" element={<PageNotFound/>}/>
+                  <Route path="heroes/:id" element={<h1>?</h1>}/>
+              </Routes>
+          </Router>
+      </SimpleBar>
   );
 };
-
-const CompactRouter = () =>
-    <Router>
-        <Routes>
-            <Route path="/login" element={<Login/>}/>
-
-            <Route path="/" element={<Private Component={<h1>home</h1>}/> }/>
-            <Route path="/cargo" element={<Private Component={<Cargo/>}/> }/>
-            <Route path="/drivers" element={<Private Component={<DriversGrid/>}/> }/>
-
-            <Route path="/transits" element={<Private Component={<Transits/>}/> }/>
-            {["/transits/add", "/transits/:id"].map(path => (
-                <Route key='transit' path={path} element={<Private Component={<TransitPage/>}/> }/>
-            ))}
-
-            <Route path="*" element={<PageNotFound/>}/>
-            <Route path="heroes/:id" element={<h1>?</h1>}/>
-        </Routes>
-    </Router>
-
 
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,69 +6,9 @@ import App from './App';
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
 
-import 'overlayscrollbars/overlayscrollbars.css';
-import {OverlayScrollbarsComponent} from "overlayscrollbars-react";
-import Scrollbar from "react-scrollbars-custom";
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Scrollbar>
-        <div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-            <div>123</div>
-        </div>
-    </Scrollbar>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
